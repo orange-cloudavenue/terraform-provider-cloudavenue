ad45c924a1c520b44c1786969653381d0170cf05
Mickael Stanislas <contact@mickael-stanislas.com>
2023-02-08 16:05:03 +0100

    chore: add helper to catch API Errors

---
+++ internal/provider/helper_api_errors.go
@@ -0,0 +1,97 @@
+package provider
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	apiclient "github.com/orange-cloudavenue/cloudavenue-sdk-go"
+)
+
+type APIError struct {
+	lastError  error
+	statusCode int
+	apiclient.ApiError
+}
+
+// Error returns the error message.
+func (e *APIError) Error() string {
+	return e.lastError.Error()
+}
+
+// Unwrap returns the underlying error.
+func (e *APIError) Unwrap() error {
+	return e.lastError
+}
+
+// GetSummary returns a summary of the error.
+func (e *APIError) GetSummary() string {
+	return fmt.Sprintf("%s (HTTP Code => %d)", e.Reason, e.GetStatusCode())
+}
+
+// GetDetail returns a detailed description of the error.
+func (e *APIError) GetDetail() string {
+	return e.Message
+}
+
+// GetStatusCode returns the HTTP status code.
+func (e *APIError) GetStatusCode() int {
+	return e.statusCode
+}
+
+// GetTerraformDiagnostic returns a Terraform Diagnostic for the error.
+func (e *APIError) GetTerraformDiagnostic() diag.Diagnostic {
+	var summary, detail string
+	if e.Reason != "" {
+		summary = e.GetSummary()
+	} else {
+		summary = fmt.Sprintf("HTTP response error: %d", e.GetStatusCode())
+	}
+	if e.Message != "" {
+		detail = e.GetDetail()
+	} else {
+		detail = e.lastError.Error()
+	}
+
+	if e.IsNotFound() {
+		return diag.NewWarningDiagnostic(summary, detail)
+	}
+	return diag.NewErrorDiagnostic(summary, detail)
+}
+
+// IsNotFound returns true if the error is a 404 Not Found error.
+func (e *APIError) IsNotFound() bool {
+	return e.GetStatusCode() == 404
+}
+
+// CheckApiError checks the HTTP response for errors and returns an APIError
+// if the response code is >= 400.
+// If the response code is < 400, nil is returned.
+func CheckApiError(err error, httpR *http.Response) *APIError {
+
+	if err == nil {
+		return nil
+	}
+
+	if httpR != nil && httpR.StatusCode >= 400 {
+
+		if e, ok := err.(apiclient.GenericSwaggerError); ok {
+			x := &APIError{
+				lastError:  err,
+				statusCode: httpR.StatusCode,
+			}
+
+			if e.Model() != nil {
+				if m, ok := e.Model().(apiclient.ApiError); ok {
+					x.ApiError = m
+				}
+			}
+
+			return x
+		}
+	} else {
+		return nil
+	}
+
+	return nil
+}
