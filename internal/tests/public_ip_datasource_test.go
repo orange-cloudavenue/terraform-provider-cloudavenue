f77f6b45a2d262fa87d5d9a1a9aa05c24bafaea7
Ere Mid <eremid@gmail.com>
2023-02-14 15:48:59 +0100

    test(tf): add public_ip datasource

--- internal/tests/public_ip_datasource_test.go
+++ internal/tests/public_ip_datasource_test.go
@@ -21,7 +21,14 @@ func TestAccPublicIPDataSource(t *testing.T) {
 				Config: testAccPublicIPDataSourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify placeholder id attribute
-					resource.TestCheckResourceAttr(dataSourceName, "id", "frangipane"),
+					resource.TestCheckResourceAttr(dataSourceName, "id", "cf9a73f3-4eb5-546b-bf2f-bdc63a439128"),
+					resource.TestCheckResourceAttr(dataSourceName, "internal_ip", "10.10.10.0/30"),
+					resource.TestCheckResourceAttr(dataSourceName, "network_config.0.edge_gateway_name", "edgeGatewayName"),
+					resource.TestCheckResourceAttr(dataSourceName, "network_config.0.uplink_ip", "196.26.50.90"),
+					resource.TestCheckResourceAttr(dataSourceName, "network_config.0.translated_ip", "10.10.10.0/30"),
+					resource.TestCheckResourceAttr(dataSourceName, "network_config.1.edge_gateway_name", "edgeGatewayName2"),
+					resource.TestCheckResourceAttr(dataSourceName, "network_config.1.uplink_ip", "196.26.50.91"),
+					resource.TestCheckResourceAttr(dataSourceName, "network_config.1.translated_ip", "10.10.10.1/30"),
 				),
 			},
 		},
