de1bfb4524cddcb5fbaabf5ef7fc8384d7485045
Ere Mid <eremid@gmail.com>
2023-02-14 15:12:00 +0100

    test(tf): add vdc and vdcs datasource

--- internal/tests/vdcs_datasource_test.go
+++ internal/tests/vdcs_datasource_test.go
@@ -17,6 +17,10 @@ func TestAccVdcsDataSource(t *testing.T) {
 				Config: testAccVdcsDataSourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "id", "frangipane"),
+					resource.TestCheckResourceAttr(dataSourceName, "vdcs.0.vdc_name", "vdc01"),
+					resource.TestCheckResourceAttr(dataSourceName, "vdcs.0.vdc_uuid", "1bb33b06-8a3b-4a2c-a077-d4771794cf3c"),
+					resource.TestCheckResourceAttr(dataSourceName, "vdcs.1.vdc_name", "vdc02"),
+					resource.TestCheckResourceAttr(dataSourceName, "vdcs.1.vdc_uuid", "9b1243ea-ca4f-4a20-9ff4-844f0584b3e7"),
 				),
 			},
 		},
