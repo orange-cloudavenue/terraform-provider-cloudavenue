4e7837769d4a8451224413941266cf27ff262259
Mickael Stanislas <contact@mickael-stanislas.com>
2023-02-15 17:25:21 +0100

    chore(helper): add PlanModifier int64 `SetDefault` `SetDefaultFunc` `SetDefaultEnvVar`

---
+++ internal/helpers/int64pm/base_default_func.go
@@ -0,0 +1,82 @@
+package int64pm
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+)
+
+// DefaultFunc is a function that can be used to set a default value for a
+// int64 attribute.
+type DefaultFunc func(context.Context, planmodifier.Int64Request, *DefaultFuncResponse)
+
+// DefaultFuncResponse is the response type for a DefaultFunc.
+type DefaultFuncResponse struct {
+	// Diagnostics report errors or warnings related to this logic. An empty
+	// or unset slice indicates success, with no warnings or errors generated.
+	Diagnostics diag.Diagnostics
+
+	// Value is the value to use by default if the attribute is not configured.
+	Value int64
+}
+
+// setDefaultFunc returns a plan modifier that conditionally requires
+// resource replacement if:
+//
+//   - The resource is planned for update.
+//   - The plan and state values are not equal.
+//   - The given function returns true. Returning false will not unset any
+//     prior resource replacement.
+func setDefaultFunc(f DefaultFunc, description, markdownDescription string) planmodifier.Int64 {
+	return defaultFuncPlanModifier{
+		f:                   f,
+		description:         description,
+		markdownDescription: markdownDescription,
+	}
+}
+
+// defaultFuncPlanModifier is an plan modifier that sets RequiresReplace
+// on the attribute if a given function is true.
+type defaultFuncPlanModifier struct {
+	f                   DefaultFunc
+	description         string
+	markdownDescription string
+}
+
+// Description returns a human-readable description of the plan modifier.
+func (m defaultFuncPlanModifier) Description(_ context.Context) string {
+	return m.description
+}
+
+// MarkdownDescription returns a markdown description of the plan modifier.
+func (m defaultFuncPlanModifier) MarkdownDescription(_ context.Context) string {
+	return m.markdownDescription
+}
+
+// PlanModifyBool implements the plan modification logic.
+func (m defaultFuncPlanModifier) PlanModifyInt64(ctx context.Context, req planmodifier.Int64Request, resp *planmodifier.Int64Response) {
+	// Do not replace if the plan and state values are equal.
+	if req.PlanValue.Equal(req.StateValue) {
+		return
+	}
+
+	// If the attribute configuration is not null, we are done here
+	if !req.ConfigValue.IsNull() && !req.ConfigValue.IsUnknown() {
+		return
+	}
+
+	// If the attribute plan is "known" and "not null", then a previous plan modifier in the sequence
+	// has already been applied, and we don't want to interfere.
+	if !req.PlanValue.IsUnknown() && !req.PlanValue.IsNull() {
+		return
+	}
+
+	funcResp := &DefaultFuncResponse{}
+
+	m.f(ctx, req, funcResp)
+
+	resp.Diagnostics.Append(funcResp.Diagnostics...)
+	resp.PlanValue = basetypes.NewInt64Value(funcResp.Value)
+}
---
+++ internal/helpers/int64pm/default_func_test.go
@@ -0,0 +1,100 @@
+package int64pm_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/orange-cloudavenue/terraform-provider-cloudavenue/internal/helpers/int64pm"
+)
+
+func TestDefaultFuncModifierPlanModifyInt64(t *testing.T) {
+	const expectedValue = 123
+
+	testCases := map[string]struct {
+		request  planmodifier.Int64Request
+		expected *planmodifier.Int64Response
+	}{
+		"null-state": {
+			// when we first create the resource, use the unknown
+			// value
+			request: planmodifier.Int64Request{
+				StateValue:  types.Int64Null(),
+				PlanValue:   types.Int64Unknown(),
+				ConfigValue: types.Int64Null(),
+			},
+			expected: &planmodifier.Int64Response{
+				PlanValue: types.Int64Value(expectedValue),
+			},
+		},
+		"known-plan": {
+			// this would really only happen if we had a plan
+			// modifier setting the value before this plan modifier
+			// got to it
+			//
+			// but we still want to preserve that value, in this
+			// case
+			request: planmodifier.Int64Request{
+				StateValue:  types.Int64Value(10),
+				PlanValue:   types.Int64Value(11),
+				ConfigValue: types.Int64Null(),
+			},
+			expected: &planmodifier.Int64Response{
+				PlanValue: types.Int64Value(11),
+			},
+		},
+		"non-null-state-unknown-plan": {
+			// this is the situation we want to preserve the state
+			// in
+			request: planmodifier.Int64Request{
+				StateValue:  types.Int64Value(10),
+				PlanValue:   types.Int64Unknown(),
+				ConfigValue: types.Int64Null(),
+			},
+			expected: &planmodifier.Int64Response{
+				PlanValue: types.Int64Value(expectedValue),
+			},
+		},
+		"unknown-config": {
+			// this is the situation in which a user is
+			// interpolating into a field. We want that to still
+			// show up as unknown, otherwise they'll get apply-time
+			// errors for changing the value even though we knew it
+			// was legitimately possible for it to change and the
+			// provider can't prevent this from happening
+			request: planmodifier.Int64Request{
+				StateValue:  types.Int64Value(10),
+				PlanValue:   types.Int64Unknown(),
+				ConfigValue: types.Int64Unknown(),
+			},
+			expected: &planmodifier.Int64Response{
+				PlanValue: types.Int64Value(expectedValue),
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			// t.Parallel()
+
+			resp := &planmodifier.Int64Response{
+				PlanValue: testCase.request.PlanValue,
+			}
+
+			x := int64pm.DefaultFunc(func(ctx context.Context, req planmodifier.Int64Request, resp *int64pm.DefaultFuncResponse) {
+				resp.Value = expectedValue
+			})
+
+			int64pm.SetDefaultFunc(x).PlanModifyInt64(context.Background(), testCase.request, resp)
+
+			if diff := cmp.Diff(testCase.expected, resp); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
