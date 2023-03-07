022d59bc6d3fd21e939226ef385425948a51242b
Mickael Stanislas <contact@mickael-stanislas.com>
2023-03-07 09:48:58 +0100

    feat: add `size_in_mb` attribute

--- internal/provider/common/vm/diskparams/size_in_mb.go
+++ internal/provider/common/vm/diskparams/size_in_mb.go
@@ -2,11 +2,36 @@ package diskparams
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 )
 
+const sizeInMBDescription = "The size of the disk in MB."
+
+/*
+SizeInMBAttribute
+
+returns a schema.Attribute with a value.
+
+  - ConflictsWith `disk_id` attribute.
+  - Computed if `disk_id` is set.
+  - Required if `name` is set.
+*/
 func SizeInMBAttribute() schema.Attribute {
 	return schema.Int64Attribute{
-		Required:            true,
-		MarkdownDescription: "The size of the disk in MB.",
+		MarkdownDescription: sizeInMBDescription,
+		Optional:            true,
+		Computed:            true,
+		PlanModifiers: []planmodifier.Int64{
+			int64planmodifier.UseStateForUnknown(),
+		},
+	}
+}
+
+// SizeInMBAttributeComputed returns a schema.Attribute with a computed value.
+func SizeInMBAttributeComputed() schema.Attribute {
+	return schema.Int64Attribute{
+		MarkdownDescription: sizeInMBDescription,
+		Computed:            true,
 	}
 }
