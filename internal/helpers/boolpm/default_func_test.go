ca48558a727ffd1c1bae2490cd98e14232fbe56c
Ere Mid <eremid@gmail.com>
2023-03-10 14:34:21 +0100

    style(dupl): fix dupl issues

--- internal/helpers/boolpm/default_func_test.go
+++ internal/helpers/boolpm/default_func_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"github.com/orange-cloudavenue/terraform-provider-cloudavenue/internal/helpers/boolpm"
 )
@@ -15,69 +14,7 @@ import (
 func TestDefaultFuncModifierPlanModifyBool(t *testing.T) {
 	expectedValue := true
 
-	testCases := map[string]struct {
-		request  planmodifier.BoolRequest
-		expected *planmodifier.BoolResponse
-	}{
-		"null-state": {
-			// when we first create the resource, use the unknown
-			// value
-			request: planmodifier.BoolRequest{
-				StateValue:  types.BoolNull(),
-				PlanValue:   types.BoolUnknown(),
-				ConfigValue: types.BoolNull(),
-			},
-			expected: &planmodifier.BoolResponse{
-				PlanValue: types.BoolValue(expectedValue),
-			},
-		},
-		"known-plan": {
-			// this would really only happen if we had a plan
-			// modifier setting the value before this plan modifier
-			// got to it
-			//
-			// but we still want to preserve that value, in this
-			// case
-			request: planmodifier.BoolRequest{
-				StateValue:  types.BoolValue(false),
-				PlanValue:   types.BoolValue(true),
-				ConfigValue: types.BoolNull(),
-			},
-			expected: &planmodifier.BoolResponse{
-				PlanValue: types.BoolValue(true),
-			},
-		},
-		"non-null-state-unknown-plan": {
-			// this is the situation we want to preserve the state
-			// in
-			request: planmodifier.BoolRequest{
-				StateValue:  types.BoolValue(false),
-				PlanValue:   types.BoolUnknown(),
-				ConfigValue: types.BoolNull(),
-			},
-			expected: &planmodifier.BoolResponse{
-				PlanValue: types.BoolValue(expectedValue),
-			},
-		},
-		"unknown-config": {
-			// this is the situation in which a user is
-			// interpolating into a field. We want that to still
-			// show up as unknown, otherwise they'll get apply-time
-			// errors for changing the value even though we knew it
-			// was legitimately possible for it to change and the
-			// provider can't prevent this from happening
-			request: planmodifier.BoolRequest{
-				StateValue:  types.BoolValue(false),
-				PlanValue:   types.BoolUnknown(),
-				ConfigValue: types.BoolUnknown(),
-			},
-			expected: &planmodifier.BoolResponse{
-				PlanValue: types.BoolValue(expectedValue),
-			},
-		},
-	}
-
-	for name, testCase := range testCases {
+	for name, testCase := range boolpmTestCases(expectedValue) {
 		name, testCase := name, testCase
 
 		t.Run(name, func(t *testing.T) {
