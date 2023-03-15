5edbb1b7dc3bbb1effdef2dc5072bbbbf5b82139
Ere Mid <eremid@users.noreply.github.com>
2023-03-15 21:08:47 +0100

    fix(network): cloudavenue_network_routed error on import (#178)

--- examples/resources/cloudavenue_network_routed/resource.tf
+++ examples/resources/cloudavenue_network_routed/resource.tf
@@ -3,10 +3,40 @@ data "cloudavenue_edgegateway" "example" {
 }
 
 resource "cloudavenue_network_routed" "example" {
-  name        = "OrgNetExample"
+  name        = "OrgNetExampleOnVDCGroup"
   description = "Org Net Example"
 
   edge_gateway_id = data.cloudavenue_edgegateway.example.id
+
+  gateway       = "192.168.1.254"
+  prefix_length = 24
+
+  dns1 = "1.1.1.1"
+  dns2 = "8.8.8.8"
+
+  dns_suffix = "example"
+
+  static_ip_pool = [
+    {
+      start_address = "192.168.1.10"
+      end_address   = "192.168.1.20"
+    }
+  ]
+}
+
+data "cloudavenue_tier0_vrfs" "example_with_vdc" {}
+
+resource "cloudavenue_edgegateway" "example_with_vdc" {
+  owner_name     = "MyVDC"
+  tier0_vrf_name = data.cloudavenue_tier0_vrfs.example_with_vdc.names.0
+  owner_type     = "vdc"
+}
+
+resource "cloudavenue_network_routed" "example" {
+  name        = "OrgNetExampleOnVDC"
+  description = "Org Net Example"
+
+  edge_gateway_id = cloudavenue_edgegateway.example_with_vdc.id
 
   gateway       = "192.168.1.254"
   prefix_length = 24
