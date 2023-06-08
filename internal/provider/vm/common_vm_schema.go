75c2fbdf0ac0fbab4d38a3100cc5567a629dc34c
Mickael Stanislas <contact@mickael-stanislas.com>
2023-06-08 19:11:49 +0200

    fix: validators in vm schema

--- internal/provider/vm/common_vm_schema.go
+++ internal/provider/vm/common_vm_schema.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"regexp"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	schemaD "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	schemaR "github.com/hashicorp/terraform-plugin-framework/resource/schema"
@@ -113,6 +115,7 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 			},
 			"deploy_os": superschema.SingleNestedAttribute{
 				Common: &schemaR.SingleNestedAttribute{
+					// TODO: add description
 					MarkdownDescription: "",
 					Computed:            true,
 				},
@@ -129,9 +132,9 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 							PlanModifiers: []planmodifier.String{
 								stringplanmodifier.RequiresReplace(),
 							},
-							// Validators: []validator.String{
-							// 	fstringvalidator.Not(stringvalidator.AlsoRequires(path.MatchRelative().AtName("boot_image_id"))),
-							// },
+							Validators: []validator.String{
+								stringvalidator.ConflictsWith(path.MatchRelative().AtParent().AtName("boot_image_id")),
+							},
 						},
 						DataSource: &schemaD.StringAttribute{
 							Computed: true,
@@ -147,7 +150,7 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 								stringplanmodifier.RequiresReplace(),
 							},
 							Validators: []validator.String{
-								stringvalidator.AlsoRequires(path.MatchRelative().AtName("vapp_template_id")),
+								stringvalidator.ConflictsWith(path.MatchRelative().AtParent().AtName("boot_image_id")),
 							},
 						},
 						DataSource: &schemaD.StringAttribute{
@@ -163,9 +166,9 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 							PlanModifiers: []planmodifier.String{
 								stringplanmodifier.RequiresReplace(),
 							},
-							// Validators: []validator.String{
-							// 	fstringvalidator.Not(stringvalidator.AlsoRequires(path.MatchRelative().AtName("vapp_template_id"))),
-							// },
+							Validators: []validator.String{
+								stringvalidator.ConflictsWith(path.MatchRelative().AtParent().AtName("vapp_template_id")),
+							},
 						},
 					},
 					"accept_all_eulas": superschema.BoolAttribute{
@@ -199,8 +202,7 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 						Resource: &schemaR.BoolAttribute{
 							Optional: true,
 							Computed: true,
-							// TODO: BUG démarre pas la VM
-							Default: booldefault.StaticBool(true),
+							Default:  booldefault.StaticBool(true),
 							PlanModifiers: []planmodifier.Bool{
 								boolplanmodifier.UseStateForUnknown(),
 							},
@@ -214,11 +216,6 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 							MarkdownDescription: "The status of the VM.",
 							Computed:            true,
 						},
-						// Resource: &schemaR.StringAttribute{
-						// 	PlanModifiers: []planmodifier.String{
-						// 		stringplanmodifier.UseStateForUnknown(),
-						// 	},
-						// },
 					},
 				},
 			},
@@ -229,7 +226,6 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 				},
 				Resource: &schemaR.SingleNestedAttribute{
 					Optional: true,
-					// Default:  objectdefault.StaticValue(resourceDefaultValue),
 				},
 				Attributes: map[string]superschema.Attribute{
 					"cpus": superschema.Int64Attribute{
@@ -260,7 +256,7 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 								int64planmodifier.UseStateForUnknown(),
 							},
 							Validators: []validator.Int64{
-								// TODO fix path
+								fint64validator.AttributeIsDivisibleByAnInteger(path.MatchRelative().AtParent().AtName("cpus")),
 							},
 						},
 					},
@@ -271,8 +267,7 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 						},
 						Resource: &schemaR.BoolAttribute{
 							Optional: true,
-							// TODO: BUG ne fonctionne pas
-							Default: booldefault.StaticBool(true),
+							Default:  booldefault.StaticBool(true),
 							PlanModifiers: []planmodifier.Bool{
 								boolplanmodifier.UseStateForUnknown(),
 							},
@@ -301,8 +296,7 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 						},
 						Resource: &schemaR.BoolAttribute{
 							Optional: true,
-							// TODO: BUG ne fonctionne pas
-							Default: booldefault.StaticBool(true),
+							Default:  booldefault.StaticBool(true),
 							PlanModifiers: []planmodifier.Bool{
 								boolplanmodifier.UseStateForUnknown(),
 							},
@@ -327,7 +321,21 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 								Resource: &schemaR.StringAttribute{
 									Required: true,
 									Validators: []validator.String{
-										stringvalidator.OneOf("vapp", "org", "none"),
+										fstringvalidator.OneOfWithDescription(
+											fstringvalidator.OneOfWithDescriptionValues{
+												Value:       "vapp",
+												Description: "A vApp network. This network is only available in your vApp structure.",
+											},
+											fstringvalidator.OneOfWithDescriptionValues{
+												Value:       "org",
+												Description: "An organization network. This network can be a network isolated or routed in your Organization.",
+											},
+											fstringvalidator.OneOfWithDescriptionValues{
+												Value:       "none",
+												Description: "No network.",
+											},
+										),
+										// stringvalidator.OneOf("vapp", "org", "none"),
 									},
 									PlanModifiers: []planmodifier.String{
 										stringplanmodifier.UseStateForUnknown(),
@@ -358,15 +366,16 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 									MarkdownDescription: "The name of the network to attach to the VM.",
 								},
 								Resource: &schemaR.StringAttribute{
-									Optional:   true,
+									Optional: true,
 									Validators: []validator.String{
-										// fstringvalidator.RequireIfAttributeIsOneOf(
-										// 	path.MatchRelative().AtParent().AtName("type"),
-										// 	[]attr.Value{
-										// 		types.StringValue("vapp"),
-										// 		types.StringValue("org"),
-										// 	},
-										// ),
+										fstringvalidator.RequireIfAttributeIsOneOf(
+											path.MatchRelative().AtParent().AtName("type"),
+											[]attr.Value{
+												types.StringValue("vapp"),
+												types.StringValue("org"),
+											},
+										),
+										// TODO: add null attribute validator if type is none
 									},
 									PlanModifiers: []planmodifier.String{
 										stringplanmodifier.UseStateForUnknown(),
@@ -381,18 +390,17 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 									MarkdownDescription: "The IP address to assign to this VM on this network.",
 								},
 								Resource: &schemaR.StringAttribute{
-									Optional:   true,
+									Optional: true,
 									Validators: []validator.String{
-										// stringvalidator.ExactlyOneOf(path.MatchRelative().AtParent().AtName("name")),
-										// fstringvalidator.RequireIfAttributeIsOneOf(
-										// 	path.MatchRelative().AtParent().AtName("ip_allocation_mode"),
-										// 	// path.MatchRelative().AtParent().AtName("ip_allocation_mode"),
-										// 	[]attr.Value{
-										// 		types.StringValue("MANUAL"),
-										// 		types.StringValue("POOL"),
-										// 	},
-										// ),
-										// TODO: error if ip_allocation_mode is not MANUAL or POOL
+										stringvalidator.ExactlyOneOf(path.MatchRelative().AtParent().AtName("name")),
+										fstringvalidator.RequireIfAttributeIsOneOf(
+											path.MatchRelative().AtParent().AtName("ip_allocation_mode"),
+											[]attr.Value{
+												types.StringValue("MANUAL"),
+												types.StringValue("POOL"),
+											},
+										),
+										// TODO : add null attribute validator if ip_allocation_mode is DHCP or NONE
 									},
 								},
 								DataSource: &schemaD.StringAttribute{
@@ -434,7 +442,24 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 									Optional: true,
 									Default:  stringdefault.StaticString("VMXNET3"),
 									Validators: []validator.String{
-										stringvalidator.OneOf("VMXNET3", "E1000E", "VMXNET3VRDMA", "SRIOVETHERNETCARD"),
+										fstringvalidator.OneOfWithDescription(
+											fstringvalidator.OneOfWithDescriptionValues{
+												Value:       "VMXNET3",
+												Description: "A VMXNET3 adapter it's an paravirtualized NIC designed for performance. VMXNET 3 offers all the features available in VMXNET 2 and adds several new features.",
+											},
+											fstringvalidator.OneOfWithDescriptionValues{
+												Value:       "E1000E",
+												Description: "A E1000E adapter it's an Emulated version of the Intel 82574 Gigabit Ethernet NIC. E1000E is the default adapter for Windows 8 and Windows Server 2012.",
+											},
+											fstringvalidator.OneOfWithDescriptionValues{
+												Value:       "VMXNET3VRDMA",
+												Description: "VMXNET3VRDMA adapter it's a E1000E adapter it's a paravirtualized NIC that supports remote direct memory access (RDMA) between virtual machines through the OFED verbs API.",
+											},
+											fstringvalidator.OneOfWithDescriptionValues{
+												Value:       "SRIOVETHERNETCARD",
+												Description: "SRIOVETHERNETCARD adapter it's a representation of a virtual function (VF) on a physical NIC with SR-IOV support.",
+											},
+										),
 									},
 								},
 							},
@@ -679,210 +704,3 @@ func vmSuperSchema(_ context.Context) superschema.Schema {
 		},
 	}
 }
-
-// func vmSchema() schema.Schema {
-// 	return schema.Schema{
-// 		MarkdownDescription: "The virtual machine (vm) resource allows you to manage a virtual machine in the CloudAvenue.",
-// 		Attributes: map[string]schema.Attribute{
-// 			"id": schema.StringAttribute{
-// 				MarkdownDescription: "The ID of the VM.",
-// 				Computed:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"vapp_name": schema.StringAttribute{
-// 				MarkdownDescription: "The vApp this VM belongs to.",
-// 				Required:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.RequiresReplace(),
-// 				},
-// 			},
-// 			"name": schema.StringAttribute{
-// 				MarkdownDescription: "The name of the VM. Unique within the vApp.",
-// 				Required:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.RequiresReplace(),
-// 				},
-// 				Validators: []validator.String{
-// 					stringvalidator.RegexMatches(regexp.MustCompile(`^[a-zA-Z0-9-]{1,80}$`), "must be between 1 and 80 characters long and can contain only letters, numbers and hyphen. It must not contain only digits."),
-// 				},
-// 			},
-// 			"computer_name": schema.StringAttribute{
-// 				MarkdownDescription: "Computer name to assign to this virtual machine. Default is `vm_name`.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				Validators: []validator.String{
-// 					stringvalidator.RegexMatches(regexp.MustCompile(`^[a-zA-Z0-9-]{1,80}$`), "must be between 1 and 80 characters long and can contain only letters, numbers and hyphen. It must not contain only digits."),
-// 				},
-// 			},
-// 			"vdc": schema.StringAttribute{
-// 				MarkdownDescription: "The name of the VDC this VM belongs to. If not specified, VDC define in provider will be used.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.RequiresReplace(),
-// 					stringplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"vapp_template_id": schema.StringAttribute{
-// 				MarkdownDescription: "The URN of the vApp template to use for this VM. Required if `boot_image_id` is not set.",
-// 				Optional:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.RequiresReplace(),
-// 				},
-// 			},
-// 			"vm_name_in_template": schema.StringAttribute{
-// 				MarkdownDescription: "The name of the VM in vApp Template to use. In cases when vApp template has more than one VM.",
-// 				Optional:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.RequiresReplace(),
-// 				},
-// 			},
-// 			"description": schema.StringAttribute{
-// 				MarkdownDescription: "Description of the VM.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"resource": schema.SingleNestedAttribute{
-// 				MarkdownDescription: "Resource configuration for the VM.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				Attributes:          vm.ResourceSchema(),
-// 				PlanModifiers: []planmodifier.Object{
-// 					objectplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"href": schema.StringAttribute{
-// 				MarkdownDescription: "VM Hyper Reference",
-// 				Computed:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"accept_all_eulas": schema.BoolAttribute{
-// 				MarkdownDescription: "Automatically accept EULA if OVA has it. Default is `false`.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				PlanModifiers: []planmodifier.Bool{
-// 					fboolplanmodifier.SetDefault(false),
-// 				},
-// 			},
-// 			"power_on": schema.BoolAttribute{
-// 				MarkdownDescription: "A boolean value stating if this VM should be powered on. Default is `true`.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				PlanModifiers: []planmodifier.Bool{
-// 					fboolplanmodifier.SetDefault(true),
-// 				},
-// 			},
-// 			"storage_profile": schema.StringAttribute{
-// 				MarkdownDescription: "Storage profile to override the default one.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"os_type": schema.StringAttribute{
-// 				MarkdownDescription: "Operating System type.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				Validators: []validator.String{
-// 					stringvalidator.OneOf(vm.GetAllOsTypes()...),
-// 				},
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"boot_image_id": schema.StringAttribute{
-// 				MarkdownDescription: "The ID of the boot image to use for this VM. Required if `vapp_template_id` is not set.",
-// 				Optional:            true,
-// 				Validators: []validator.String{
-// 					stringvalidator.ExactlyOneOf(path.MatchRoot("vapp_template_id")),
-// 				},
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.RequiresReplace(),
-// 				},
-// 			},
-// 			"network_dhcp_wait_seconds": schema.Int64Attribute{
-// 				MarkdownDescription: "Optional number of seconds to try and wait for DHCP IP (valid for `network` block only)",
-// 				Optional:            true,
-// 				Validators: []validator.Int64{
-// 					int64validator.AtLeast(0),
-// 				},
-// 			},
-// 			"networks": schema.ListNestedAttribute{
-// 				MarkdownDescription: "A block to define network interface. Multiple can be used.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				NestedObject: schema.NestedAttributeObject{
-// 					Attributes: vm.NetworkSchema(),
-// 				},
-// 				PlanModifiers: []planmodifier.List{
-// 					listplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"disks": schema.SetNestedAttribute{
-// 				MarkdownDescription: "A list of disks attached to this VM.",
-// 				Computed:            true,
-// 				NestedObject: schema.NestedAttributeObject{
-// 					Attributes: diskInternalExternalSchema(),
-// 				},
-// 			},
-// 			"expose_hardware_virtualization": schema.BoolAttribute{
-// 				MarkdownDescription: "Expose hardware-assisted CPU virtualization to guest OS. Default is `false`.",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				PlanModifiers: []planmodifier.Bool{
-// 					fboolplanmodifier.SetDefault(false),
-// 					boolplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"guest_properties": vm.GuestPropertiesSchema(),
-// 			"customization": schema.SingleNestedAttribute{
-// 				MarkdownDescription: "Guest customization block. ",
-// 				Optional:            true,
-// 				Computed:            true,
-// 				Attributes:          vm.CustomizationSchema(),
-// 				PlanModifiers: []planmodifier.Object{
-// 					objectplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"prevent_update_power_off": schema.BoolAttribute{
-// 				MarkdownDescription: "`true` if the update of resource should fail when virtual machine power off needed. Default is `false`.",
-// 				Optional:            true,
-// 				PlanModifiers: []planmodifier.Bool{
-// 					fboolplanmodifier.SetDefault(false),
-// 				},
-// 			},
-// 			"sizing_policy_id": schema.StringAttribute{
-// 				MarkdownDescription: "VM placement policy ID. Has to be assigned to Org VDC.",
-// 				Optional:            true,
-// 				Computed:            true, // As it can get populated automatically by VDC default policy
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"placement_policy_id": schema.StringAttribute{
-// 				MarkdownDescription: "VM placement policy ID. Has to be assigned to Org VDC.",
-// 				Optional:            true,
-// 				Computed:            true, // As it can get populated automatically by VDC default policy
-// 				PlanModifiers: []planmodifier.String{
-// 					stringplanmodifier.UseStateForUnknown(),
-// 				},
-// 			},
-// 			"status_code": schema.Int64Attribute{
-// 				MarkdownDescription: "Shows the status code of the VM",
-// 				Computed:            true,
-// 			},
-// 			"status_text": schema.StringAttribute{
-// 				MarkdownDescription: "Shows the status text of the VM",
-// 				Computed:            true,
-// 			},
-// 		},
-// 	}
-// }
