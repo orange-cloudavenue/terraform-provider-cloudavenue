685f03ab0933dbe61be451e6e7e71a62a40f36fc
Mickael Stanislas <contact@mickael-stanislas.com>
2023-06-22 22:55:53 +0200

    chore: add acceptance test for `cloudavenue_vm_affinity_rule` datasource

--- internal/tests/vm/vm_affinity_rule_datasource_test.go
+++ internal/tests/vm/vm_affinity_rule_datasource_test.go
@@ -11,7 +11,64 @@ import (
 //go:generate tf-doc-extractor -filename $GOFILE -example-dir ../../../examples -test
 const testAccVMAffinityRuleDataSourceConfig = `
 data "cloudavenue_vm_affinity_rule" "example" {
+	id = cloudavenue_vm_affinity_rule.example.id
 }
+
+resource "cloudavenue_vm_affinity_rule" "example" {
+	name     = "example-affinity-rule"
+	polarity = "Affinity"
+  
+	vm_ids = [
+	  cloudavenue_vm.example.id,
+	  cloudavenue_vm.example2.id,
+	]
+  }
+  
+  resource "cloudavenue_vm" "example" {
+	  name      = "example-vm"
+	  vapp_name = cloudavenue_vapp.example.name
+	  deploy_os = {
+		vapp_template_id = data.cloudavenue_catalog_vapp_template.example.id
+	  }
+	  settings = {
+		customization = {
+		  auto_generate_password = true
+		}
+	  }
+	  resource = {
+	  }
+	
+	  state = {
+	  }
+  }
+  
+  resource "cloudavenue_vm" "example2" {
+	  name      = "example-vm2"
+	  vapp_name = cloudavenue_vapp.example.name
+	  deploy_os = {
+		vapp_template_id = data.cloudavenue_catalog_vapp_template.example.id
+	  }
+	  settings = {
+		customization = {
+		  auto_generate_password = true
+		}
+	  }
+	  resource = {
+	  }
+	
+	  state = {
+	  }
+  }
+  
+  data "cloudavenue_catalog_vapp_template" "example" {
+	  catalog_name = "Orange-Linux"
+	  template_name    = "debian_10_X64"
+  }
+  
+  resource "cloudavenue_vapp" "example" {
+	  name = "vapp_example"
+	  description = "This is a example vapp"
+  }
 `
 
 func TestAccVmAffinityRuleDatasourceDataSource(t *testing.T) {
@@ -26,6 +83,12 @@ func TestAccVmAffinityRuleDatasourceDataSource(t *testing.T) {
 				Config: testAccVMAffinityRuleDataSourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet(dataSourceName, "id"),
+					resource.TestCheckResourceAttr(dataSourceName, "name", "example-affinity-rule"),
+					resource.TestCheckResourceAttr(dataSourceName, "polarity", "Affinity"),
+					resource.TestCheckResourceAttr(dataSourceName, "required", "true"),
+					resource.TestCheckResourceAttr(dataSourceName, "enabled", "true"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "vm_ids.0"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "vm_ids.1"),
 				),
 			},
 		},
