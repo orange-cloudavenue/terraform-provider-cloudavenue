6213414d8b4e7a2c165d97bf54b4bf826030daad
DUBREUIL Valentin <valentin.dubreuil@orange.com>
2023-07-24 12:11:41 +0200

    chore(vcda): Add schema/types

--- internal/tests/vcda/vcda_ip_resource_test.go
+++ internal/tests/vcda/vcda_ip_resource_test.go
@@ -1,9 +1,11 @@
 package vcda
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	tests "github.com/orange-cloudavenue/terraform-provider-cloudavenue/internal/tests/common"
 )
@@ -13,6 +15,17 @@ resource "cloudavenue_vcda_ip" "example" {
 	ip_address = "10.0.0.1"
 }
 `
+
+func testAccVCDAImportStateIDFunc(resourceName string) resource.ImportStateIdFunc {
+	return func(s *terraform.State) (string, error) {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return "", fmt.Errorf("Not found: %s", resourceName)
+		}
+
+		return rs.Primary.Attributes["ip_address"], nil
+	}
+}
 
 func TestAccVCDAResource(t *testing.T) {
 	const resourceName = "cloudavenue_vcda_ip.example"
@@ -30,7 +43,7 @@ func TestAccVCDAResource(t *testing.T) {
 			{
 				ResourceName:      resourceName,
 				ImportState:       true,
-				ImportStateId:     "10.0.0.1",
+				ImportStateIdFunc: testAccVCDAImportStateIDFunc(resourceName),
 				ImportStateVerify: true,
 				Destroy:           true,
 			},
