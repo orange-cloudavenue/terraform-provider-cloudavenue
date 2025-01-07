9038ee4bb6c0997b7b6e246b35d8c2de37e84ae8
Mickael Stanislas <contact@mickael-stanislas.com>
2025-01-07 11:48:53 +0100

    chore: synced file(s) with orange-cloudavenue/workflows (#939)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- internal/provider/edgegw/app_port_profile_datasource.go
+++ internal/provider/edgegw/app_port_profile_datasource.go
@@ -1,3 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Orange
+ * SPDX-License-Identifier: Mozilla Public License 2.0
+ *
+ * This software is distributed under the MPL-2.0 license.
+ * the text of which is available at https://www.mozilla.org/en-US/MPL/2.0/
+ * or see the "LICENSE" file for more details.
+ */
+
 package edgegw //nolint:dupl // This is a datasource, it is normal to have similar code to the other datasource.
 
 import (
