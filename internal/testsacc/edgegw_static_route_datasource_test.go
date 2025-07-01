1754631d5a6b165510fd5e8aec68a7b14ab4d42b
David MICHENEAU <david.micheneau@orange.com>
2025-07-01 09:02:09 +0200

    refactor: TestACC datasource and resource for edgegateway static route

--- internal/testsacc/edgegw_static_route_datasource_test.go
+++ internal/testsacc/edgegw_static_route_datasource_test.go
@@ -10,31 +10,61 @@
 package testsacc
 
 import (
+	"context"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/orange-cloudavenue/terraform-provider-cloudavenue/internal/helpers/testsacc"
 )
 
-const testAccStaticRouteDataSourceConfig = `
-data "cloudavenue_edgegateway_static_route" "example" {
-	edge_gateway_id = cloudavenue_edgegateway.example_with_vdc.id
-	name = cloudavenue_edgegateway_static_route.example.name
+var _ testsacc.TestACC = &EdgeGatewayStaticRouteDataSource{}
+
+const (
+	EdgeGatewayStaticRouteDataSourceName = testsacc.ResourceName("data.cloudavenue_edgegateway_static_route")
+)
+
+type EdgeGatewayStaticRouteDataSource struct{}
+
+func NewEdgeGatewayStaticRouteDataSourceTest() testsacc.TestACC {
+	return &EdgeGatewayStaticRouteDataSource{}
 }
-`
 
-func TestAccStaticRouteDataSource(t *testing.T) {
-	dataSourceName := "data.cloudavenue_edgegateway_static_route.example"
+func (r *EdgeGatewayStaticRouteDataSource) GetResourceName() string {
+	return EdgeGatewayStaticRouteDataSourceName.String()
+}
 
+func (r *EdgeGatewayStaticRouteDataSource) DependenciesConfig() (resp testsacc.DependenciesConfigResponse) {
+	resp.Append(GetResourceConfig()[EdgeGatewayStaticRouteResourceName]().GetDefaultConfig)
+	return
+}
+
+func (r *EdgeGatewayStaticRouteDataSource) Tests(_ context.Context) map[testsacc.TestName]func(ctx context.Context, resourceName string) testsacc.Test {
+	return map[testsacc.TestName]func(ctx context.Context, resourceName string) testsacc.Test{
+		"basic": func(_ context.Context, _ string) testsacc.Test {
+			return testsacc.Test{
+				Create: testsacc.TFConfig{
+					TFConfig: `
+					data "cloudavenue_edgegateway_static_route" "example" {
+					  edge_gateway_id = cloudavenue_edgegateway.example_with_vdc.id
+					  name = cloudavenue_edgegateway_static_route.example.name
+					}
+					`,
+					Checks: []resource.TestCheckFunc{
+						resource.TestCheckResourceAttrSet("data.cloudavenue_edgegateway_static_route.example", "id"),
+						resource.TestCheckResourceAttrSet("data.cloudavenue_edgegateway_static_route.example", "network"),
+						resource.TestCheckResourceAttrSet("data.cloudavenue_edgegateway_static_route.example", "next_hop"),
+					},
+				},
+			}
+		},
+	}
+}
+
+func TestAccEdgeGatewayStaticRouteDataSource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Read testing
-			{
-				// Apply test
-				Config: ConcatTests(testAccEdgeGatewayResourceConfig, testAccStaticRouteResourceConfig, testAccStaticRouteDataSourceConfig),
-				Check:  staticRouteTestCheck(dataSourceName),
-			},
-		},
+		Steps:                    testsacc.GenerateTests(&EdgeGatewayStaticRouteDataSource{}),
 	})
 }
