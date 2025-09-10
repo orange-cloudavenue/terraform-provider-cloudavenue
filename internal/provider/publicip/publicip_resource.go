0c7bdba25738077d1a279831c3a9b922ffc2f4b7
Mickael Stanislas <contact@mickael-stanislas.com>
2025-09-10 16:42:14 +0200

    feat: refactor datas `_edgegateways` to sdkv2 (#1166)
    Co-authored-by: David MICHENEAU <david.micheneau@orange.com>

--- internal/provider/publicip/publicip_resource.go
+++ internal/provider/publicip/publicip_resource.go
@@ -24,7 +24,6 @@ import (
 	"github.com/orange-cloudavenue/cloudavenue-sdk-go-v2/api/edgegateway/v1"
 	"github.com/orange-cloudavenue/cloudavenue-sdk-go-v2/types"
 	"github.com/orange-cloudavenue/cloudavenue-sdk-go/pkg/urn"
-
 	"github.com/orange-cloudavenue/terraform-provider-cloudavenue/internal/client"
 	"github.com/orange-cloudavenue/terraform-provider-cloudavenue/internal/metrics"
 )
