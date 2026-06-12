8dbd25d2a01b83cf9a8cb3393ab2d5fcb0048c65
Abdoul Hamid Coulibaly <abdoulhamid.coulibaly.ext@orange.com>
2026-06-12 17:29:01 +0200

    test(vdcg): update acceptance tests for network context profile and firewall
    - Update firewall datasource and resource tests
    - Add network context profile resource tests
    - Align tests with updated schema and validators

--- internal/testsacc/vdcg_network_context_profile_resource_test.go
+++ internal/testsacc/vdcg_network_context_profile_resource_test.go
@@ -42,7 +42,7 @@ func (r *VDCGNetworkContextProfileResource) DependenciesConfig() (resp testsacc.
 
 func (r *VDCGNetworkContextProfileResource) Tests(_ context.Context) map[testsacc.TestName]func(ctx context.Context, resourceName string) testsacc.Test {
 	return map[testsacc.TestName]func(ctx context.Context, resourceName string) testsacc.Test{
-		// Basic profile with multiple App IDs, no sub-attributes.
+		// Basic profile with multiple App IDs.
 		testNameExample: func(_ context.Context, resourceName string) testsacc.Test {
 			return testsacc.Test{
 				CommonChecks: []resource.TestCheckFunc{
@@ -57,20 +57,19 @@ func (r *VDCGNetworkContextProfileResource) Tests(_ context.Context) map[testsac
 						vdc_group_name = cloudavenue_vdcg.example.name
 						name           = {{ generate . "name" }}
 						description    = {{ generate . "description" }}
-						attribute = [
-							{ app_id = "SSH", sub_attribute = [] },
-							{ app_id = "DNS", sub_attribute = [] },
-						]
+						app_id = {
+							values = ["SSH", "DNS"]
+						}
 					}`),
 					Checks: []resource.TestCheckFunc{
 						resource.TestCheckResourceAttr(resourceName, "name", testsacc.GetValueFromTemplate(resourceName, "name")),
 						resource.TestCheckResourceAttr(resourceName, "description", testsacc.GetValueFromTemplate(resourceName, "description")),
-						resource.TestCheckResourceAttr(resourceName, "attribute.#", "2"),
-						resource.TestCheckResourceAttr(resourceName, "attribute.0.app_id", "SSH"),
-						resource.TestCheckResourceAttr(resourceName, "attribute.1.app_id", "DNS"),
+						resource.TestCheckResourceAttr(resourceName, "app_id.values.#", "2"),
+						resource.TestCheckTypeSetElemAttr(resourceName, "app_id.values.*", "SSH"),
+						resource.TestCheckTypeSetElemAttr(resourceName, "app_id.values.*", "DNS"),
 					},
 				},
-				// Update: change description and switch to a single App ID with sub-attributes.
+				// Update: switch to SSL with TLS_VERSION sub-attribute.
 				Updates: []testsacc.TFConfig{
 					{
 						TFConfig: testsacc.GenerateFromTemplate(resourceName, `
@@ -78,25 +77,21 @@ func (r *VDCGNetworkContextProfileResource) Tests(_ context.Context) map[testsac
 							vdc_group_name = cloudavenue_vdcg.example.name
 							name           = {{ get . "name" }}
 							description    = {{ generate . "description" }}
-							attribute = [
-								{
-									app_id = "SSL"
-									sub_attribute = [
-										{
-											type   = "TLS_VERSION"
-											values = ["TLS_V12", "TLS_V13"]
-										}
-									]
-								}
-							]
+							app_id = {
+								values = ["SSL"]
+								sub_attribute = [
+									{
+										type   = "TLS_VERSION"
+										values = ["TLS_V12", "TLS_V13"]
+									}
+								]
+							}
 						}`),
 						Checks: []resource.TestCheckFunc{
-							resource.TestCheckResourceAttr(resourceName, "name", testsacc.GetValueFromTemplate(resourceName, "name")),
-							resource.TestCheckResourceAttr(resourceName, "description", testsacc.GetValueFromTemplate(resourceName, "description")),
-							resource.TestCheckResourceAttr(resourceName, "attribute.#", "1"),
-							resource.TestCheckResourceAttr(resourceName, "attribute.0.app_id", "SSL"),
-							resource.TestCheckResourceAttr(resourceName, "attribute.0.sub_attribute.#", "1"),
-							resource.TestCheckResourceAttr(resourceName, "attribute.0.sub_attribute.0.type", "TLS_VERSION"),
+							resource.TestCheckResourceAttr(resourceName, "app_id.values.#", "1"),
+							resource.TestCheckTypeSetElemAttr(resourceName, "app_id.values.*", "SSL"),
+							resource.TestCheckResourceAttr(resourceName, "app_id.sub_attribute.#", "1"),
+							resource.TestCheckResourceAttr(resourceName, "app_id.sub_attribute.0.type", "TLS_VERSION"),
 						},
 					},
 				},
