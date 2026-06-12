b5865ffbbf3eab425a36dc7cdbc061373cc7bcf1
Abdoul Hamid Coulibaly <abdoulhamid.coulibaly.ext@orange.com>
2026-06-12 17:27:54 +0200

    refactor(edgegw): add validators to firewall network_context_profile_ids
    - Import setvalidator and fstringvalidator for validation
    - Add URN prefix validation to network_context_profile_ids
    - Ensure only valid NetworkContextProfile URNs are accepted

--- internal/provider/edgegw/firewall_schema.go
+++ internal/provider/edgegw/firewall_schema.go
@@ -23,6 +23,7 @@ import (
 
 	superschema "github.com/orange-cloudavenue/terraform-plugin-framework-superschema"
 	supertypes "github.com/orange-cloudavenue/terraform-plugin-framework-supertypes"
+	fstringvalidator "github.com/orange-cloudavenue/terraform-plugin-framework-validators/stringvalidator"
 
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -37,7 +38,10 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+
+	"github.com/orange-cloudavenue/cloudavenue-sdk-go/pkg/urn"
 )
 
 func firewallSchema(_ context.Context) superschema.Schema {
@@ -209,14 +213,19 @@ func firewallSchema(_ context.Context) superschema.Schema {
 							Computed: true,
 						},
 					},
-					"network_context_profile_ids": superschema.SuperSetAttributeOf[string]{
-						Common: &schemaR.SetAttribute{
-							MarkdownDescription: "A set of Network Context Profile IDs (Layer 7). Use `data.cloudavenue_edgegateway_network_context_profile` to look up a SYSTEM/PROVIDER profile by name, or reference a `cloudavenue_edgegateway_network_context_profile` resource directly. Leaving it empty means `Any` (all).",
-							ElementType:         types.StringType,
+				"network_context_profile_ids": superschema.SuperSetAttributeOf[string]{
+					Common: &schemaR.SetAttribute{
+						MarkdownDescription: "A set of Network Context Profile IDs (Layer 7). Use `data.cloudavenue_edgegateway_network_context_profile` to look up a SYSTEM/PROVIDER profile by name, or reference a `cloudavenue_edgegateway_network_context_profile` resource directly. Leaving it empty means `Any` (all).",
+						ElementType:         types.StringType,
+					},
+					Resource: &schemaR.SetAttribute{
+						Optional: true,
+						Validators: []validator.Set{
+							setvalidator.ValueStringsAre(
+								fstringvalidator.PrefixContains(urn.NetworkContextProfile.String()),
+							),
 						},
-						Resource: &schemaR.SetAttribute{
-							Optional: true,
-						},
+					},
 						DataSource: &schemaD.SetAttribute{
 							Computed: true,
 						},
