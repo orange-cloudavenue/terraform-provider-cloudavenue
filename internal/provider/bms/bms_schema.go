e19ce9ef3e10bc1a19050c96ab0185acf9999048
dimahc <52035010+dimahc@users.noreply.github.com>
2026-09-03 10:29:28 +0000

    chore: fix piled license headers

--- internal/provider/bms/bms_schema.go
+++ internal/provider/bms/bms_schema.go
@@ -3,7 +3,7 @@
 
 /*
  * SPDX-FileCopyrightText: Copyright (c) 2026 Orange
- * SPDX-License-Identifier: Mozilla Public License 2.0
+ * SPDX-License-Identifier: MPL-2.0
  *
  * This software is distributed under the MPL-2.0 license.
  * the text of which is available at https://www.mozilla.org/en-US/MPL/2.0/
@@ -12,7 +12,7 @@
 
 /*
  * SPDX-FileCopyrightText: Copyright (c) 2025 Orange
- * SPDX-License-Identifier: Mozilla Public License 2.0
+ * SPDX-License-Identifier: MPL-2.0
  *
  * This software is distributed under the MPL-2.0 license.
  * the text of which is available at https://www.mozilla.org/en-US/MPL/2.0/
--- tools/tools.go
+++ tools/tools.go
@@ -1,9 +1,11 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026 Orange
 // SPDX-License-Identifier: MPL-2.0
 
+//go:build tools
+
 /*
  * SPDX-FileCopyrightText: Copyright (c) 2026 Orange
- * SPDX-License-Identifier: Mozilla Public License 2.0
+ * SPDX-License-Identifier: MPL-2.0
  *
  * This software is distributed under the MPL-2.0 license.
  * the text of which is available at https://www.mozilla.org/en-US/MPL/2.0/
@@ -12,14 +14,12 @@
 
 /*
  * SPDX-FileCopyrightText: Copyright (c) 2025 Orange
- * SPDX-License-Identifier: Mozilla Public License 2.0
+ * SPDX-License-Identifier: MPL-2.0
  *
  * This software is distributed under the MPL-2.0 license.
  * the text of which is available at https://www.mozilla.org/en-US/MPL/2.0/
  * or see the "LICENSE" file for more details.
  */
-
-//go:build tools
 
 package tools
 
